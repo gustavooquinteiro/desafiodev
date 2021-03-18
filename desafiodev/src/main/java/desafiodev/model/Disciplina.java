e05e76607d972a558b8d9458fd4b7083dafd19e9
Gustavo Quinteiro <gustavo.quinteiro@ufba.br>
2021-03-18 06:35:12 -0300

    Refatorando classe Disciplina

--- desafiodev/src/main/java/desafiodev/model/Disciplina.java
+++ desafiodev/src/main/java/desafiodev/model/Disciplina.java
@@ -1,9 +1,24 @@
 package desafiodev.model;
 
-public class Disciplina {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Disciplina implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	private String nome;
+	@Column
 	private double valor;
+	@ManyToOne
+	@JoinColumn(name = "aluno")
+	private Aluno aluno;
 
 	public String getNome() {
 		return nome;
@@ -19,6 +34,50 @@ public class Disciplina {
 
 	public void setValor(double valor) {
 		this.valor = valor;
+	}
+
+	public Aluno getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(Aluno aluno) {
+		this.aluno = aluno;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aluno == null) ? 0 : aluno.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(valor);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (aluno == null) {
+			if (other.aluno != null)
+				return false;
+		} else if (!aluno.equals(other.aluno))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(valor) != Double.doubleToLongBits(other.valor))
+			return false;
+		return true;
 	}
 	
 	
