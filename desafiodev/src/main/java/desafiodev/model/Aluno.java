58be73c0a004e76e80dce095782259482a0c7e73
Gustavo Quinteiro <gustavo.quinteiro@ufba.br>
2021-03-18 06:34:15 -0300

    Refatorando classe Aluno

--- desafiodev/src/main/java/desafiodev/model/Aluno.java
+++ desafiodev/src/main/java/desafiodev/model/Aluno.java
@@ -2,11 +2,27 @@ package desafiodev.model;
 
 import java.util.ArrayList;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+@Entity
 public class Aluno extends Usuario{
-	
+
+	private static final long serialVersionUID = 1L;
+
+	@Column
 	private String nome;
+	
+	@OneToMany (mappedBy = "aluno", targetEntity = Disciplina.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+
 	private ArrayList<Disciplina> disciplinas;
+	
+	@OneToMany (mappedBy = "aluno", targetEntity = Disciplina.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private ArrayList<Disciplina> alternativas;
+	
+	@Column
 	private double valorASerPago;
 	
 	
@@ -33,6 +49,46 @@ public class Aluno extends Usuario{
 	}
 	public void setValorASerPago(double valorASerPago) {
 		this.valorASerPago = valorASerPago;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((alternativas == null) ? 0 : alternativas.hashCode());
+		result = prime * result + ((disciplinas == null) ? 0 : disciplinas.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(valorASerPago);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (alternativas == null) {
+			if (other.alternativas != null)
+				return false;
+		} else if (!alternativas.equals(other.alternativas))
+			return false;
+		if (disciplinas == null) {
+			if (other.disciplinas != null)
+				return false;
+		} else if (!disciplinas.equals(other.disciplinas))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(valorASerPago) != Double.doubleToLongBits(other.valorASerPago))
+			return false;
+		return true;
 	}
 	
 	
