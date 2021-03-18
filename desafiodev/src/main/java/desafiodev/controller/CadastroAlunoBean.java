c004cc36be44d30edd1a22eb7eab8d2146cfd025
Gustavo Quinteiro <gustavo.quinteiro@ufba.br>
2021-03-18 09:05:56 -0300

    Adicionando escopos do CDI

--- desafiodev/src/main/java/desafiodev/controller/CadastroAlunoBean.java
+++ desafiodev/src/main/java/desafiodev/controller/CadastroAlunoBean.java
@@ -2,12 +2,15 @@ package desafiodev.controller;
 
 import java.io.Serializable;
 
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import desafiodev.model.Aluno;
 import desafiodev.service.CadastroAlunoService;
 import desafiodev.util.jsf.FacesUtil;
-
+@Named
+@ViewScoped
 public class CadastroAlunoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
