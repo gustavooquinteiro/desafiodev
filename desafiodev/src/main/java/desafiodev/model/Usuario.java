b1d543557aba380b597cff0fa5d189ef16489b6f
Gustavo Quinteiro <gustavo.quinteiro@ufba.br>
2021-03-19 09:45:28 -0300

    Consertando caminho do header

--- desafiodev/src/main/java/desafiodev/model/Usuario.java
+++ desafiodev/src/main/java/desafiodev/model/Usuario.java
@@ -1,6 +1,7 @@
 package desafiodev.model;
 
 import java.io.Serializable;
+import java.security.MessageDigest;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -34,9 +35,25 @@ public class Usuario implements Serializable {
 		return senha;
 	}
 	public void setSenha(String senha) {
-		this.senha = senha;
+		this.senha = criptografa(senha);
 	}
 	
 	
-	
+	private String criptografa(String senha) {
+		try {
+			MessageDigest algorithm = MessageDigest.getInstance("MD5");
+
+			byte messageDigest[] = algorithm.digest(senha.getBytes("UTF-8"));
+			StringBuilder hexString = new StringBuilder();
+
+			for (byte b : messageDigest) {
+				hexString.append(String.format("%02X", 0xFF & b));
+			}
+
+			return hexString.toString().toLowerCase();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
